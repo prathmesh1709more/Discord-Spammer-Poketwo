a4b16b3bc0a1496ec8e8bbaba414d8e3be1dd562
prathmesh1709more <93133504+prathmesh1709more@users.noreply.github.com>
2022-09-01 21:17:20 +0530

    Update main.py
    ..

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from webserver import keep_alive
 import requests
 
-channelID = PUT THE CHANNEL ID
+channelID = 1014412483345272832
 headers = {
     "authorization":
-    "YOUR TOKEN HERE"
+    "MTAxNDU2NTExNDMyMzY2OTA3Mg.G0_Axp.1v1HqfoI5Nk8VvusquCC-aNYfHUbb-bLnDPg2Y"
 }
 keep_alive()
 file = open("text.txt", "r")
